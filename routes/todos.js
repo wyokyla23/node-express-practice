ea06e3c84fcd1fdc63b92037ec7d9bf1cc9b35b6
wyokyla23 <wyokyla23@gmail.com>
2020-11-28 13:18:30 -0600

    feat: added delete and patch methods for todos list

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,8 @@ import { v4 as uuidv4 } from 'uuid'
 
 const router = Express.Router()
 
-const todos = [
+//should be const
+let todos = [
   {
     task: "shave grandma",
     id: 1,
@@ -25,16 +26,32 @@ router.post('/', (request, response) => {
   const todo = request.body
   const todoWithId = { ...todo, id: uuidv4() }
   todos.push(todoWithId)
-
+  // const newTodos = [...todos, todoWithId]
   response.send(todos)
   // response.send(`${todo} was added to the database.`)
 })
-
 
 // /todos/2 => request.params { id: 2 }
 router.get('/:id', (request, response) => {
   const { id } = request.params
   const foundTodo = todos.find((todo) => todo.id === Number(id))
+  response.send(foundTodo)
+})
+
+router.delete('/:id', (request, response) => {
+  const { id } = request.params
+  // const updatedTodos = todos.filter((todo) => todo.id != Number(id))
+  todos = todos.filter((todo) => todo.id != Number(id))
+  response.send(todos)
+})
+
+router.patch('/:id', (request, response) => {
+  const { id } = request.params
+  const { task, completed } = request.body
+  const foundTodo = todos.find((todo) => todo.id === Number(id))
+  if (task) foundTodo.task = task
+  if (completed) foundTodo.completed = completed
+
   response.send(foundTodo)
 })
 
